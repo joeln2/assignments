870258c7033343f30c298f0d7e88c428debf8316
joeln2 <129252192+joeln2@users.noreply.github.com>
2023-12-09 05:57:32 +0530

    Update anagram.js
    wrote a function to check if anagram or not

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+    const n = str1.length;
+    if (n !== str2.length) {
+        return false;
+    }
+    let c = Array(26).fill(0);
+    for (let i = 0; i < n; i++) {
+        c[str1[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        c[str2[i].charCodeAt(0) - 'a'.charCodeAt(0)]--;
+    }
+    for (let char of c) {
+        if (char !== 0) {
+            return false;
+        }
+    }
+    return true;
 }
-
-module.exports = isAnagram;
